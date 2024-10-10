56de1504bf5eddf1daea0021fd4c8ab54369d3f0
Iván García <66884918+IvanGH4@users.noreply.github.com>
2024-10-10 14:58:15 -0300

    add ability to have composed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,15 @@
 import createMiddleware from "next-intl/middleware";
+import { NextRequest } from "next/server";
+
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+export default async function middleware(request: NextRequest) {
+  // Create and call the next-intl middleware (example)
+  const handleI18nRouting = createMiddleware(routing);
+  const response = handleI18nRouting(request);
+
+  return response;
+}
 
 export const config = {
   // Match only internationalized pathnames
